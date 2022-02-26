a60483c944c0854fec731a62d25543170cb8885e
Arash <47273556+arashnrim@users.noreply.github.com>
2022-02-26 23:03:13 +0800

    Configure ESLint + Prettier and run linter

--- backend/app.js
+++ backend/app.js
@@ -1,37 +1,39 @@
-const express = require("express")
-const app = express()
-require("dotenv").config()
-const { calcDRMBPrice, calcDSGDPrice } = require("./priceRetrieval")
-const { getDRMBBalance, getDSGDBalance } = require("./getBalance")
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const { calcDRMBPrice, calcDSGDPrice } = require("./priceRetrieval");
+const { getDRMBBalance, getDSGDBalance } = require("./getBalance");
 
-app.get("/", (req, res) => { res.sendStatus(200) })
+app.get("/", (req, res) => {
+  res.sendStatus(200);
+});
 
 app.get("/retrievePrice/drmb", async (req, res) => {
-    let DRMBPrice = await calcDRMBPrice()
-    res.setHeader("Content-Type", "application/json")
-    res.end(JSON.stringify({ drmbPrice: DRMBPrice }))
-})
+  let DRMBPrice = await calcDRMBPrice();
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify({ drmbPrice: DRMBPrice }));
+});
 
 app.get("/retrievePrice/dsgd", async (req, res) => {
-    let DSGDPrice = await calcDSGDPrice()
-    res.setHeader("Content-Type", "application/json")
-    res.end(JSON.stringify({ dsgdPrice: DSGDPrice }))
-})
+  let DSGDPrice = await calcDSGDPrice();
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify({ dsgdPrice: DSGDPrice }));
+});
 
-app.get("/getBalance/drmb", async(req, res) => {
-    let balance = await getDRMBBalance()
-    res.setHeader("Content-Type", "application/json")
-    res.end(JSON.stringify({ balance: balance }))
-})
+app.get("/getBalance/drmb", async (req, res) => {
+  let balance = await getDRMBBalance();
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify({ balance: balance }));
+});
 
-app.get("/getBalance/dsgd", async(req, res) => {
-    let balance = await getDSGDBalance()
-    res.setHeader("Content-Type", "application/json")
-    res.end(JSON.stringify({ balance: balance }))
-})
+app.get("/getBalance/dsgd", async (req, res) => {
+  let balance = await getDSGDBalance();
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify({ balance: balance }));
+});
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.debug(`Express is running on port ${port}.`)
-})
+  console.debug(`Express is running on port ${port}.`);
+});
--- backend/getBalance.js
+++ backend/getBalance.js
@@ -1,28 +1,38 @@
-const Web3 = require('web3');
+const Web3 = require("web3");
 
-let pancakeSwapContract = "0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3".toLowerCase();
+let pancakeSwapContract =
+  "0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3".toLowerCase();
 const web3 = new Web3("https://data-seed-prebsc-1-s1.binance.org:8545/");
 
 const dsgdAddress = "0x25d1d454084Df3B23B0C982bc2E4CC2Dd06a7F12";
-const drmbAddress ="0x54ec93A799ea75c2ED8685d6310f5bF0c37c1bE4";
+const drmbAddress = "0x54ec93A799ea75c2ED8685d6310f5bF0c37c1bE4";
 const holderAddress = "0xd3999C07e2c09BDecC7c245E68cDF5a726c88863"; //User wallet address
 
 const abiJson = [
-    {"constant":true,"inputs":[{"name":"who","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},
+  {
+    constant: true,
+    inputs: [{ name: "who", type: "address" }],
+    name: "balanceOf",
+    outputs: [{ name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
 ];
 
 async function getDSGDBalance() {
-    const contract = new web3.eth.Contract(abiJson, dsgdAddress);
-    const balance = await contract.methods.balanceOf(holderAddress).call();
-    return(balance/1000000000000000000);
+  const contract = new web3.eth.Contract(abiJson, dsgdAddress);
+  const balance = await contract.methods.balanceOf(holderAddress).call();
+  return balance / 1000000000000000000;
 }
 
 async function getDRMBBalance() {
-    const contract = new web3.eth.Contract(abiJson, drmbAddress);
-    const balance = await contract.methods.balanceOf(holderAddress).call();
-    return(balance/1000000000000000000);
+  const contract = new web3.eth.Contract(abiJson, drmbAddress);
+  const balance = await contract.methods.balanceOf(holderAddress).call();
+  return balance / 1000000000000000000;
 }
 
 module.exports = {
-    getDSGDBalance, getDRMBBalance
-}
+  getDSGDBalance,
+  getDRMBBalance,
+};
