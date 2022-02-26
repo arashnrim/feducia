8aece0eed87dfdeaef44c428171e75defdf213a4
Arash <47273556+arashnrim@users.noreply.github.com>
2022-02-27 01:38:39 +0800

    Add detailed information for frontend in getTransactions
    Co-authored-by: Karl Steven Velasco Orjalo <karlorjalo@gmail.com>

--- backend/getTransactions.js
+++ backend/getTransactions.js
@@ -1,50 +1,69 @@
 const https = require("https");
 const { retrieveAmountFromInputData } = require("./translateInputData");
 const { checkCurrency } = require("./currencies");
-let userAddress = "0xd3999C07e2c09BDecC7c245E68cDF5a726c88863"; //User address, Metamask login dependent
-let url = `https://testnet.bscscan.com/api?module=account&action=tokentx&address=${userAddress}&startblock=0&endblock=99999999&offset=10&sort=asc`;
-let data = [];
-https
-  .get(url, (res) => {
-    let body = "";
 
-    res.on("data", (chunk) => {
-      body += chunk;
-    });
+function getTransactions(userAddress) {
+  let url = `https://testnet.bscscan.com/api?module=account&action=tokentx&address=${userAddress}&startblock=0&endblock=99999999&offset=10&sort=asc`;
+  let data = [];
+  https
+    .get(url, (res) => {
+      let body = "";
 
-    res.on("end", () => {
-      try {
-        let json = JSON.parse(body);
-        for (let i = 1; i < 5; i++) {
-          var userStatus = "";
-          var currency = "";
-          try {
-            const amount = retrieveAmountFromInputData(
-              json.result[json.result.length - i].input
-            );
-            let sender = json.result[json.result.length - i].from;
-            if (sender.toLowerCase() != userAddress.toLowerCase()) {
-              userStatus = "received";
-            } else {
-              userStatus = "sent";
+      res.on("data", (chunk) => {
+        body += chunk;
+      });
+
+      res.on("end", () => {
+        try {
+          let json = JSON.parse(body);
+          for (let i = 1; i < 5; i++) {
+            var userStatus = "";
+            var currency = "";
+            var flow = "";
+            try {
+              const amount = retrieveAmountFromInputData(
+                json.result[json.result.length - i].input
+              );
+              let sender = json.result[json.result.length - i].from;
+              if (sender.toLowerCase() != userAddress.toLowerCase()) {
+                userStatus = "received";
+              } else {
+                userStatus = "sent";
+              }
+              let contractAddress =
+                json.result[json.result.length - i].contractAddress;
+              currency = checkCurrency(contractAddress);
+              if (userStatus == "received") {
+                let moneyFlow = json.result[json.result.length - i].from;
+                flow = `from ${moneyFlow}`;
+              } else if (userStatus == "sent") {
+                let moneyFlow = json.result[json.result.length - i].from;
+                flow = `to ${moneyFlow}`;
+              }
+              let txHash = json.result[json.result.length - i].contractAddress;
+              data.push({
+                amount: amount,
+                type: userStatus,
+                currency: currency,
+                flow: flow,
+                txHash: txHash,
+              });
+            } catch (error) {
+              continue;
             }
-            let contractAddress =
-              json.result[json.result.length - i].contractAddress;
-            currency = checkCurrency(contractAddress);
-            data.push({
-              amount: amount,
-              type: userStatus,
-              currency: currency,
-            });
-          } catch (error) {
-            continue;
           }
+        } catch (error) {
+          console.error(error.message);
         }
-      } catch (error) {
-        console.error(error.message);
-      }
+
+        return data;
+      });
+    })
+    .on("error", (error) => {
+      console.error(error.message);
     });
-  })
-  .on("error", (error) => {
-    console.error(error.message);
-  });
+}
+
+module.exports = {
+  getTransactions,
+};
