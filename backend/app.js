de9768143a0534866e358645440e9f78fa26d721
Arash <47273556+arashnrim@users.noreply.github.com>
2022-02-26 21:31:01 +0800

    Add basic backend implementation
    Co-authored-by: Karl Steven Velasco Orjalo <karlorjalo@gmail.com>

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const app = express()
+const port = 5000
+const { calcDRMBPrice, calcDSGDPrice } = require("./priceRetrieval")
+const { getBalance } = require("./getBalance")
+
+app.get("/retrievePrice/drmb", async (req, res) => {
+    let DRMBPrice = await calcDRMBPrice()
+    res.setHeader("Content-Type", "application/json")
+    res.end(JSON.stringify({ drmbPrice: DRMBPrice }))
+})
+
+app.get("/retrievePrice/drmb", async (req, res) => {
+    let DSGDPrice = await calcDSGDPrice()
+    res.setHeader("Content-Type", "application/json")
+    res.end(JSON.stringify({ dsgdPrice: DSGDPrice }))
+})
+
+app.get("/getBalance", async(req, res) => {
+    let balance = await getBalance()
+    res.setHeader("Content-Type", "application/json")
+    res.end(JSON.stringify({ balance: balance }))
+})
+
+app.listen(port, () => {
+    console.debug(`Express is running on port ${port}.`)
+})
