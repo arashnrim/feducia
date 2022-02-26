9d2fb247ddb246c0b2ed3685fd4ac785ddd1c5b9
Arash <47273556+arashnrim@users.noreply.github.com>
2022-02-26 16:53:50 +0800

    Remove sign in implementation for future use

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,36 +1,7 @@
-import { supabase } from "../utils/supabaseClient";
-import { useState } from "react";
-import { Router } from "next/router";
-
 const Home = () => {
-  const signIn = async (email, password) => {
-    const { error } = await supabase.auth.signIn({
-      email: email,
-      password: password,
-    });
-
-    if (error) {
-      alert(error.error_description || error.message);
-    } else {
-      Router.push("/home");
-    }
+  const signIn = () => {
+    // TODO: Implement linking here
   };
-
-  const signUp = async (email, password) => {
-    const { error } = await supabase.auth.signUp({
-      email: email,
-      password: password,
-    });
-
-    if (error) {
-      alert(error.error_description || error.message);
-    } else {
-      Router.push("/home");
-    }
-  };
-
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
 
   return (
     <main className="flex w-screen h-screen">
@@ -43,28 +14,12 @@ const Home = () => {
       <section className="w-[30vw] bg-gray-100 flex justify-center items-center">
         <div className="w-full p-10 mx-10 space-y-5 bg-white shadow-lg h-1/2 rounded-xl">
           <h1 className="text-3xl font-bold">Log in</h1>
-          <input
-            className="w-full h-12 px-5 border border-gray-300 rounded-lg"
-            placeholder="Username"
-            onChange={(event) => setUsername(event.target.value)}
-          ></input>
-          <input
-            className="w-full h-12 px-5 border border-gray-300 rounded-lg"
-            type="password"
-            placeholder="Password"
-            onChange={(event) => setPassword(event.target.value)}
-          ></input>
+          <p>Select the provider you use for your wallet.</p>
           <button
             className="bg-[#216de2] h-12 w-full rounded-lg hover:bg-[#1850a5] text-white font-bold"
-            onClick={() => signIn(username, password)}
+            onClick={() => signIn()}
           >
-            Confirm
-          </button>
-          <button
-            className="bg-[#216de2] h-12 w-full rounded-lg hover:bg-[#1850a5] text-white font-bold"
-            onClick={() => signUp(username, password)}
-          >
-            Create account
+            Log in with Metamask
           </button>
         </div>
       </section>
