56cb4483f089fcfb635a9241d15e12b483fa98ee
Arash <47273556+arashnrim@users.noreply.github.com>
2022-02-26 17:29:30 +0800

    Improve home page implementation

--- frontend/pages/home.jsx
+++ frontend/pages/home.jsx
@@ -6,8 +6,33 @@ const Home = () => {
   ];
 
   return (
-    <main className="flex px-5 space-x-20 bg-[#216de2] h-screen">
-      <section className="w-[50vw] h-fit bg-white shadow-lg rounded-xl p-10 space-y-5">
+    <main className="grid grid-cols-5 p-10 gap-y-10 gap-x-20 bg-[#216de2] min-h-screen h-fit">
+      <section className="col-span-3 p-10 space-y-5 bg-white shadow-lg h-[50vh] rounded-xl">
+        <h1 className="text-3xl font-bold">Total Asset Balance</h1>
+      </section>
+      <section className="col-span-2 p-10 space-y-5 bg-white shadow-lg rounded-xl">
+        <h1 className="text-3xl font-bold">Transfer money</h1>
+        <div className="flex flex-row space-x-2">
+          <input
+            className="w-full h-12 px-5 border border-gray-300 rounded-lg"
+            placeholder="Send amount?"
+          ></input>
+          <select className="border border-gray-300 rounded-lg">
+            <option value="USD">USD</option>
+            <option value="RMB">RMB</option>
+            <option value="SGD">SGD</option>
+          </select>
+        </div>
+        <input
+          className="w-full h-12 px-5 border border-gray-300 rounded-lg"
+          placeholder="Send to?"
+        ></input>
+        <button className="bg-[#216de2] h-12 w-full rounded-lg hover:bg-[#1850a5] text-white font-bold">
+          Confirm
+        </button>
+      </section>
+
+      <section className="col-span-3 p-10 space-y-5 bg-white shadow-lg h-fit rounded-xl">
         <h1 className="text-3xl font-bold">Recent Transactions</h1>
         <div className="p-5 divide-y-2 divide-gray-200">
           {recentTransactions.map((transaction) => (
@@ -17,29 +42,6 @@ const Home = () => {
               {transaction.person}
             </p>
           ))}
-        </div>
-      </section>
-      <section className="w-[50vw] h-fit bg-white shadow-lg rounded-xl p-10">
-        <div className="p-10 mx-10 space-y-5 ">
-          <h1 className="text-3xl font-bold">Transfer money</h1>
-          <div className="flex flex-row space-x-2">
-            <input
-              className="w-full h-12 px-5 border border-gray-300 rounded-lg"
-              placeholder="Send amount?"
-            ></input>
-            <select className="border border-gray-300 rounded-lg">
-              <option value="USD">USD</option>
-              <option value="RMB">RMB</option>
-              <option value="SGD">SGD</option>
-            </select>
-          </div>
-          <input
-            className="w-full h-12 px-5 border border-gray-300 rounded-lg"
-            placeholder="Send to?"
-          ></input>
-          <button className="bg-[#216de2] h-12 w-full rounded-lg hover:bg-[#1850a5] text-white font-bold">
-            Confirm
-          </button>
         </div>
       </section>
     </main>
